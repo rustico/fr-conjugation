e0c7441631d37544622b6d6c80760157a8fe06ba
rustico <nmbases@gmail.com>
2013-06-29 22:33:48 -0300

    Init

---
+++ scraper/conjugaison/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for conjugaison project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'conjugaison'
+
+SPIDER_MODULES = ['conjugaison.spiders']
+NEWSPIDER_MODULE = 'conjugaison.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'conjugaison (+http://www.yourdomain.com)'
